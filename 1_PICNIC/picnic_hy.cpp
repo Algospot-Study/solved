f7b7a9dcb5776deb40555b6fe3f990e59548b1ba
portal3046 <portal3046@gmail.com>
2016-01-15 16:56:36 +0900

    Create picnic_hy.cpp

---
+++ 1_PICNIC/picnic_hy.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<bool> isFinished;
+vector<vector<bool>> isFriend;
+int n;
+
+bool isDone(vector<bool> &isFinished) {
+    for (bool i : isFinished) {
+        if (i == false) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int mate(vector<bool> &isFinished, int startFlag) {
+    int ret = 0;
+    if (isDone(isFinished)) return 1;
+    for (int i = startFlag; i < n; i++) {
+        if (isFinished[i] == false) {
+            for (int j = i + 1; j < n; ++j) {
+                if ((isFriend[i][j] == true) && (isFinished[j] == false)) {
+                    isFinished[i] = true;
+                    isFinished[j] = true;
+                    ret += mate(isFinished, i + 1);
+                    isFinished[i] = false;
+                    isFinished[j] = false;
+                }
+            }
+        }
+    }
+    return ret;
+}
+
+int main() {
+    int c;
+    cin >> c;
+    for (int i = 0; i < c; ++i) {
+        int m;
+        cin >> n >> m;
+
+        isFriend = vector<vector<bool>>(n, vector<bool>(n, false));
+
+        for (int j = 0; j < m; ++j) {
+            int a, b;
+            cin >> a >> b;
+
+            isFriend[a][b] = true;
+            isFriend[b][a] = true;
+        }
+
+        isFinished = vector<bool>(n, false);
+
+        cout << mate(isFinished, 0) << endl;
+    }
+
+    return 0;
+}
